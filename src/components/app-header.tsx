6f5c4ee9609d3e59616577c785795052b1ba1316
Denis Lit <denislitviak@gmail.com>
2023-02-20 19:19:32 +0200

    improve header styling

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -2,20 +2,15 @@ import { NavLink } from "react-router-dom";
 
 export function AppHeader() {
     return (
-        <header className="app-header flex-row space-between">
-            <h1 className="logo">meme by denis lit</h1>
-            <ul className="page-nav clean-list flex-row">
-                <li><NavLink className="link" to="/">Gallery</NavLink></li>
-                <li><NavLink className="link" to="/">About</NavLink></li>
-                <li><NavLink className="link" to="/">My Memes</NavLink></li>
-            </ul>
-            <div className="dropdown-nav">
-                <button className="dropbtn hamburger">☰</button>
-                <div className="dropdown-content">
-                    <NavLink className="link" to="/">Gallery</NavLink>
-                    <NavLink className="link" to="/">About</NavLink>
-                </div>
-            </div>
+        <header className="main-layout">
+            <section className="app-header flex-row space-between">
+                <h1 className="logo">meme by denis lit</h1>
+                <ul className="page-nav clean-list flex-row">
+                    <li><NavLink className="link" to="/">Gallery</NavLink></li>
+                    <li><NavLink className="link" to="/">About</NavLink></li>
+                    <li><NavLink className="link" to="/">My Memes</NavLink></li>
+                </ul>
+            </section>
         </header>
     )
 }
